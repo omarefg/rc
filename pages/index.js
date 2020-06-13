2f34cd4dfd161344e968f2230d5c4ce5ca227839
Omar Flores Grimontt <dev@omarefg.com>
2020-06-13 13:01:01 -0500

    Adde cube visualization

--- pages/index.js
+++ pages/index.js
@@ -1,207 +1,124 @@
+import { useState } from 'react'
 import Head from 'next/head'
 
+const colors = {
+  red: '#b80a31',
+  blue: '#0044af',
+  orange: '#ff5700',
+  yellow: '#ffd600',
+  green: '#009c46',
+  white: '#ffffff',
+}
+
+const data = [
+  Array(9).fill(colors.orange),
+  Array(9).fill(colors.blue),
+  Array(9).fill(colors.white),
+  Array(9).fill(colors.red),
+  Array(9).fill(colors.yellow),
+  Array(9).fill(colors.green),
+];
+
 export default function Home() {
+  const [faces, setFaces] = useState(data)
+
   return (
-    <div className="container">
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Rubik's Cube</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className="main">
+      <div className="container">
+        <div>
+          {faces.slice(0, 1).map((face, faceIndex) => (
+            <div key={faceIndex} className="face">
+                {face.map((color, colorIndex) => (
+                  <div
+                    key={colorIndex}
+                    className="item"
+                    style={{ background: color }}
+                  />
+                ))}
+            </div>
+          ))}
         </div>
+        <div>
+          {faces.slice(1, 4).map((face, faceIndex) => (
+            <div key={faceIndex} className="face">
+                {face.map((color, colorIndex) => (
+                  <div
+                    key={colorIndex}
+                    className="item"
+                    style={{ background: color }}
+                  />
+                ))}
+            </div>
+          ))}
+        </div>
+        <div>
+          {faces.slice(4, 5).map((face, faceIndex) => (
+            <div key={faceIndex} className="face">
+                {face.map((color, colorIndex) => (
+                  <div
+                    key={colorIndex}
+                    className="item"
+                    style={{ background: color }}
+                  />
+                ))}
+            </div>
+          ))}
+        </div>
+        <div>
+          {faces.slice(5).map((face, faceIndex) => (
+            <div key={faceIndex} className="face">
+                {face.map((color, colorIndex) => (
+                  <div
+                    key={colorIndex}
+                    className="item"
+                    style={{ background: color }}
+                  />
+                ))}
+            </div>
+          ))}
+        </div>
+      </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
-        .container {
+        .main {
+          display: flex;
           min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
+          width: 100vw;
+          align-items: center;
+          justify-content: center;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
         }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
+        .container {
           display: flex;
           align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
         }
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
+        .face {
+          display: grid;
+          grid-template-columns: repeat(3,50px);
+          grid-template-rows: repeat(3,50px);
+          gap: 5px;
+          background-color: black;
         }
 
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+        .item {
+          width: 50px;
+          height: 50px;
         }
       `}</style>
 
-      <style jsx global>{`
-        html,
+      <style global jsx>{`
         body {
-          padding: 0;
+          background-color: #333;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
         }
       `}</style>
     </div>
